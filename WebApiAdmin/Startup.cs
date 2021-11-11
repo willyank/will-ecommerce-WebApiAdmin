0fe5f164cbfbbe921984130f4630730d7e253513
willyan klumb <wklumb@grupogbi.com>
2021-11-11 08:49:28 -0300

    removendo metodo

--- WebApiAdmin/Startup.cs
+++ WebApiAdmin/Startup.cs
@@ -93,17 +93,5 @@ namespace WebApiAdmin
             });
         }
 
-        private async Task<string> ReadBodyFromRequest(HttpRequest request)
-        {
-            // Ensure the request's body can be read multiple times (for the next middlewares in the pipeline).
-            request.EnableBuffering();
-
-            using var streamReader = new StreamReader(request.Body, leaveOpen: true);
-            var requestBody = await streamReader.ReadToEndAsync();
-
-            // Reset the request's body stream position for next middleware in the pipeline.
-            request.Body.Position = 0;
-            return requestBody;
-        }
     }
 }
